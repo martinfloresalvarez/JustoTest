798dade452125b7061d0b371fbe20d48e05132d2
Martin Flores <martinfloresalvarez@hotmail.com>
2022-10-03 01:22:30 -0500

    Updates

---
+++ JustoTest/Util/UIConstants.swift
@@ -0,0 +1,21 @@
+//
+//  UIConstants.swift
+//  JustoTest
+//
+//  Created by Martin Flores Alvarez on 02/10/22.
+//
+
+import Foundation
+import UIKit
+
+enum ConstantsSpacingDesign {
+  static let horizontalLabelPadding: CGFloat = 33
+  static let verticalLabelSpacing: CGFloat = 11
+}
+
+enum ConstantsImageSizes {
+  static let imageUserSize: CGFloat = 44
+  static let horizontalPadding: CGFloat = 22
+  static let verticalPadding: CGFloat = 11
+
+}
